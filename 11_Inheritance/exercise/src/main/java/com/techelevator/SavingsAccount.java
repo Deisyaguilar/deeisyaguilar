b8af9b82ab810e5bab4fd0c3c76d8f7e681e261e
Deisy <Deeisyaguilar@gmail.com>
2024-05-11 21:35:14 -0400

    Inheritance Exercise

---
+++ 11_Inheritance/exercise/src/main/java/com/techelevator/SavingsAccount.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+public class SavingsAccount extends BankAccount{
+
+    public SavingsAccount(String accountHolderName, String accountNumber) {
+        super(accountHolderName, accountNumber);
+    }
+
+    public SavingsAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName, accountNumber, balance);
+    }
+    @Override
+    public int withdraw(int amountToWithdraw){
+        if(getBalance() - amountToWithdraw >= 2) {
+            super.withdraw(amountToWithdraw);
+        }if(getBalance() < 150 && amountToWithdraw > 0) {
+            super.withdraw(2);
+        }
+        return getBalance();
+    }
+}
