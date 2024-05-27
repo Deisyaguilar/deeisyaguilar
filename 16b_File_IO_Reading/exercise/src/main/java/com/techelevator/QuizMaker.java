e8011cb0b2ea5e47adf40afb3d5b45b619a3fdd9
Deisy <Deeisyaguilar@gmail.com>
2024-05-27 18:20:48 -0400

    File IO Reading Exercise

--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,6 +1,12 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+
 
 public class QuizMaker {
 
@@ -14,6 +20,63 @@ public class QuizMaker {
 
 	public void run() {
 		/* Your code goes here */
+
+		System.out.println("Enter the fully qualified name of the file to read in for quiz question");
+		String inputFile = userInput.nextLine();
+		File userFile = new File(inputFile);
+		List<String> quizQuestions = new ArrayList<>();
+
+
+		try(Scanner fileInput = new Scanner(userFile)){
+			while(fileInput.hasNextLine()){
+				String fileLine = fileInput.nextLine();
+				quizQuestions.add(fileLine);
+			}
+			int answerNumber = 1;
+			String questions = null;
+			int correctAnswer = 0;
+			int score = 0;
+
+			List<String> answers = new ArrayList<>();
+
+			for (String quizQuestion : quizQuestions){
+				System.out.println(quizQuestion);
+			if(quizQuestion != null && !quizQuestion.isEmpty()){
+				String[] split = quizQuestion.split("\\|");
+				questions = split[0];
+
+				for (int i = 1; i < split.length; i++){
+					if(split[i].endsWith("*")){
+						correctAnswer = i;
+						answers.add(split[i].replace("*", ""));
+					}else{
+						answers.add(split[i]);
+					}
+				}
+				System.out.println(questions);
+				for(String answer: answers){
+					System.out.println(answerNumber + ". " + answer);
+					answerNumber++;
+				}
+				int userInputAnswer = userInput.nextInt();
+				System.out.println("your answer: "+ userInputAnswer);
+				if(userInputAnswer == correctAnswer){
+					System.out.println("RIGHT!");
+					score++;
+				}else{
+					System.out.println("WRONG!");
+				}
+				answerNumber = 1;
+				answers.clear();
+				System.out.println("You got "+ score +" correct out of "+quizQuestions.size());
+			}
+			}
+
+		}catch (FileNotFoundException e){
+			System.err.println("File not found" + userFile.getAbsolutePath());
+			throw new RuntimeException(e);
+		}
+
 
 
 	}
--- 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ 16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -11,11 +13,59 @@ public class WordSearch {
 		WordSearch wordSearch = new WordSearch();
 		wordSearch.run();
 	}
+	// collect users input and perform search
+	// look for search word
+	// include line number
+	// display a message to the user about the error
+	// make sure that  the caseSensitiveSearch_LineNumbers and caseSensitiveSearch_LineText tests now pass
 
 	public void run() {
 		/* Your code goes here */
 
+		System.out.println("What is the fully qualified name of this file that should be searched?");
+		String inputFile = userInput.nextLine();
+		File userFile = new File(inputFile);
 
-	}
+
+		if(userFile.exists() == false){
+			System.out.println(userFile + " does not exist.");
+			System.exit(0);
+		} else if (userFile.isFile() == false){
+			System.out.println(userFile + " is not a file.");
+			System.exit(0);
+		}
+		System.out.println("What is the search word you are looking for?");
+		String searchWord = userInput.nextLine();
+
+		if(searchWord == null || searchWord.isEmpty()){
+			System.out.println(searchWord + " the word is not valid");
+			System.exit(0);
+		}
+		System.out.println("Should the search be case sensitive? (Y/N)");
+		String caseSensitive = userInput.nextLine();
+
+
+		int lineNumber = 0;
+
+		try (Scanner userFileScanner = new Scanner(userFile)){
+			while(userFileScanner.hasNextLine()){
+				String lineFile = userFileScanner.nextLine();
+				lineNumber++;
+
+				if(caseSensitive.equalsIgnoreCase("Y")) {
+					if (lineFile.contains(searchWord)) {
+						System.out.println(lineNumber + ") " + lineFile);
+					}
+				}else if (lineFile.toLowerCase().contains(searchWord.toLowerCase())) {
+						System.out.println(lineNumber + ") " + lineFile);
+
+				}
+			}
+		} catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 }
