f2e6a8bd035ebeeca93be2cff7036466f6d42d5d
Deisy <Deeisyaguilar@gmail.com>
2024-06-03 00:12:37 -0400

    File IO Writing Exercise

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,10 +1,21 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
 
     // Use this scanner for all user input. Don't create additional Scanners with System.in
+    /*The search word
+The word to replace the search word with
+The source file
+This must be an existing file. If the user enters an invalid source file, the program indicates this to the user and exits.
+The destination file
+The program creates a copy of the source file with the requested replacements at this location. If the file already exists,
+it must be overwritten. If the user enters an invalid destination file, the program indicates this to the user and exits.
+     */
     private final Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -14,6 +25,30 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String fileSource = userInput.nextLine();
+        File file = new File(fileSource);
+
+        System.out.println("What is the destination file?");
+        String destination = userInput.nextLine();
+        File destinationFile = new File(destination);
+
+        try (Scanner dataInput = new Scanner(file); PrintWriter dataOutput = new PrintWriter(destination)) {
+            while(dataInput.hasNextLine()) {
+                String lineOfInput = dataInput.nextLine();
+                dataOutput.println(lineOfInput.replace(searchWord, replacementWord));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Cannot open the file for writing.");
+        }
+
+        userInput.close();
 
 
     }
--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
@@ -12,10 +14,40 @@ public class FizzWriter {
 		fizzWriter.run();
 	}
 
+	/*If the number is divisible by 3, print Fizz.
+If the number is divisible by 5, print Buzz.
+If the number is divisible by 3 and 5, print FizzBuzz.
+Otherwise, print the number.
+The program must prompt the user for the following values:
+
+The destination file
+If the file already exists, it must be overwritten.
+If the user enters an invalid destination file, the program indicates this and exits.
+	 */
+
 	public void run() {
 		/* Your code goes here */
 
+		String fileName = userInput.nextLine();
 
-	}
+		try(PrintWriter writer = new PrintWriter(fileName)) {
+			for (int i = 1; i <= 300; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					writer.println("FizzBuzz");
+				} else if (i % 5 == 0) {
+					writer.println("Buzz");
+				} else if (i % 3 == 0 ) {
+					writer.println("Fizz");
+				} else {
+					writer.println(i);
+				}
+				}
+			}
 
+		catch (FileNotFoundException e) {
+			System.err.println("Cannot open file");
+            throw new RuntimeException(e);
+        }
+
+    }
 }
